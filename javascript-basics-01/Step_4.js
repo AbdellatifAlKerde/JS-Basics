3cb24538ada8c481196dc1ab368ddc5f216befb3
Abdellatif Al Kerde <boudikerde0@gmail.com>
2022-12-08 09:18:15 +0200

    Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,11 @@
+var button = document.getElementById("validate");
+
+button.onclick = function alertTest() {
+  userName = document.getElementById("name").value;
+  surname = document.getElementById("surname").value;
+  city = document.getElementById("city").value;
+
+  alert(
+    "Name: " + userName + "\n" + "Surname: " + surname + "\n" + "City: " + city
+  );
+};
