6a5003c430fe053d5cf1eb82b475b6e93db8cbc4
Abdellatif Al Kerde <boudikerde0@gmail.com>
2022-12-08 12:16:55 +0200

    Step_2 use 1 Function

--- javascript-basics-03/step 2/Step_2.js
+++ javascript-basics-03/step 2/Step_2.js
@@ -1,11 +1,22 @@
 var show = document.getElementById("show");
 var hide = document.getElementById("hide");
 var text = document.getElementById("texte");
+var link = document.querySelectorAll("a");
 
-show.addEventListener("click", function () {
-  text.style = "display: block;";
-});
+// show.addEventListener("click", function () {
+//   text.style = "display: block;";
+// });
 
-hide.addEventListener("click", function () {
-  text.style = "display: none;";
+// hide.addEventListener("click", function () {
+//   text.style = "display: none;";
+// });
+
+link.forEach((e) => {
+  e.addEventListener("click", (e) => {
+    if (e.target.id == "show") {
+      text.style = "display: block;";
+    } else {
+      text.style = "display: none;";
+    }
+  });
 });
