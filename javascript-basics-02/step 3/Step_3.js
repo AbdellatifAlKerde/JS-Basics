6b849f74a9c44d59ccc14c2d4af180bf0a4233a7
Abdellatif Al Kerde <boudikerde0@gmail.com>
2022-12-08 09:51:04 +0200

    Step_3

---
+++ javascript-basics-02/step 3/Step_3.js
@@ -0,0 +1,10 @@
+var yourName = document.querySelector("#name");
+var div = document.createElement("div");
+div.setAttribute("id", "container");
+document.body.appendChild(div);
+
+var container = document.getElementById("container");
+
+yourName.addEventListener("change", function () {
+  container.innerHTML = yourName.value;
+});
